68e67d7035b89dabd4895672e56af854031b5c79
ahmed <ahmedissaten@gmail.com>
2024-07-09 23:04:29 +0200

    import generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """
 module for async comperhention
 """
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None]:
     """
     Import async_generator from the previous task
     then write a coroutine called async_comprehension that takes no arguments.
